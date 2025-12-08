a1badaee165266a1cbb975f540a8a9be2eb55380
indra yadav <yindrakumar2061@gmail.com>
2025-12-09 03:07:45 +0545

    background image

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import background from "../assets/background.png";
+
 export default function landingPage() {
 
   const routeTo = useNavigate();
