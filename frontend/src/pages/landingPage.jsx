3c686ab9e58512b3f52db6e22548dc6e56e2cda5
indra yadav <yindrakumar2061@gmail.com>
2025-12-09 02:52:00 +0545

    background image

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import bg from "../assets/background.png";
 export default function landingPage() {
 
   const routeTo = useNavigate();
@@ -11,7 +12,7 @@ export default function landingPage() {
     routeTo("/asas");
   }
   return (
-    <div className="landingpagecontainer">
+    <div className="landingpagecontainer" style={{ backgroundImage: `url(${bg})` }}>
       <nav>
         <div className="navHeader">
           <h2>APNA VIDEO CALL</h2>
