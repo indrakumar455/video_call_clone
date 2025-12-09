028d345eb52134c2fbd7de12b80c768bf73e74d9
indra yadav <yindrakumar2061@gmail.com>
2025-12-09 13:58:04 +0545

    some change

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,8 +18,8 @@ const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000));
 app.use(cors());
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit : "40kb", extended: true}));
+app.use(express.json());//{limit:"40kb"}
+app.use(express.urlencoded({ extended: true})); //limit : "40kb",
 
 app.use('/api/v1/users', userRoutes);
 
